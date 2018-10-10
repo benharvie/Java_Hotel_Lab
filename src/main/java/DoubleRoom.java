82ed0b5ff156f59afe80ead8925c22b584060b6a
Ben <benharvie94@gmail.com>
2018-10-10 10:19:13 +0100

    All DoubleRoom tests passing

--- src/main/java/DoubleRoom.java
+++ src/main/java/DoubleRoom.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class DoubleRoom {
+    private int roomNumber;
+    private int capacity;
+    private ArrayList<Guest> roomComplement;
+
+    public DoubleRoom(int roomNumber){
+        this.roomNumber = roomNumber;
+        this.capacity = 2;
+        this.roomComplement = new ArrayList<>();
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getComplement() {
+        return roomComplement.size();
+    }
 }
--- src/test/java/DoubleRoomTest.java
+++ src/test/java/DoubleRoomTest.java
@@ -1,2 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DoubleRoomTest {
+    DoubleRoom room2;
+
+    @Before
+    public void before(){
+        room2 = new DoubleRoom(2);
+    }
+
+    @Test
+    public void hasRoomNumber(){
+        assertEquals(2, room2.getRoomNumber());
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(2, room2.getCapacity());
+    }
+
+    @Test
+    public void hasComplement(){
+        assertEquals(0, room2.getComplement());
+    }
 }
