fb3864178f0b675eaeb58af9438e6b0fdd1c55f6
Ben <benharvie94@gmail.com>
2018-10-10 10:21:38 +0100

    All ConferenceRoom tests passing

--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class ConferenceRoom {
+    private int roomNumber;
+    private int capacity;
+    private ArrayList<Guest> roomComplement;
+
+    public ConferenceRoom(int roomNumber){
+        this.roomNumber = roomNumber;
+        this.capacity = 12;
+        this.roomComplement = new ArrayList<>();
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getComplement() {
+        return roomComplement.size();
+    }
 }
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,2 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceRoomTest {
+    ConferenceRoom room3;
+
+    @Before
+    public void before(){
+        room3 = new ConferenceRoom(3);
+    }
+
+    @Test
+    public void hasRoomNumber(){
+        assertEquals(3, room3.getRoomNumber());
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(12, room3.getCapacity());
+    }
+
+    @Test
+    public void hasComplement(){
+        assertEquals(0, room3.getComplement());
+    }
 }
