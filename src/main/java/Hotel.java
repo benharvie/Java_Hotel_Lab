bc362012063535c59a03915ba3ff08f757c1e5ab
Ben <benharvie94@gmail.com>
2018-10-10 10:41:58 +0100

    Hotel hasName test passing

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Hotel {
+    private String name;
+    private HashMap<String, ArrayList<Object>> roomList;
+
+    public Hotel(String name, HashMap<String, ArrayList<Object>> roomList) {
+        this.name = name;
+        this.roomList = roomList;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
 public class HotelTest {
+    Hotel hotel;
+    SingleRoom room1;
+    DoubleRoom room2;
+    ConferenceRoom room3;
+    HashMap<String, ArrayList<Object>> roomList;
+
+    @Before
+    public void before(){
+        roomList = new HashMap<>();
+        hotel = new Hotel("Dump Tower", roomList);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Dump Tower", hotel.getName());
+    }
 }
